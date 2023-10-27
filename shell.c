9aaa7fabde91597f7df95fdcff370806ce98e503
harshey1103 <harshit1557@gmail.com>
2023-10-27 21:17:24 +0530

    implemented write1 and updated read

--- shell.c
+++ shell.c
@@ -1,44 +1,92 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define MAX_INPUT_LENGTH 50
 int CURR_NUM_FILES = 0;
 
 struct FileAccessInfo {
-    char filename[50];
-    int readers;
-    int writers;
+  char *filename;
+  int readers;
+  int writers;
 };
 
 struct FileAccessInfo files[16];
 
-void execute_read_command(const char *file_name){
-  FILE *file = fopen(file_name, "rb"); // Open the file in binary read mode
-
-  if (file == NULL) {
-    // The file doesn't exist, so create it
-    file = fopen(file_name, "wb");
-    if (file == NULL) {
-      perror("Error creating the file");
-      return; // Return -1 to indicate an error
+int update_writers(char *file_name){
+  for (int i = 0; i < CURR_NUM_FILES; i++)
+  {
+    if(files[i].filename==file_name){
+      files[i].writers++;
+      return i;
     }
   }
+  return CURR_NUM_FILES;
+}
+
+int update_readers(char *file_name){
+  for (int i = 0; i < CURR_NUM_FILES; i++)
+  {
+    if(files[i].filename==file_name){
+      files[i].readers++;
+      return i;
+    }
+  }
+  return CURR_NUM_FILES;
+}
+
+void execute_read_command(char *file_name){
+  FILE *file = fopen(file_name, "rb"); // Open the file in binary read mode
 
   // Seek to the end of the file and get the file size
   fseek(file, 0, SEEK_END);
   long size = ftell(file);
-  // for (int i = 0; i < CURR_NUM_FILES; i++)
-  // {
-  //   if()
-  // }
-  
-  printf("read %s of %lu\n", file_name, size);
-  
+  int ind = update_readers(file_name);
   fclose(file);
+
+  if(ind==CURR_NUM_FILES)
+  {
+    files[CURR_NUM_FILES].filename=file_name;
+    files[CURR_NUM_FILES].readers=1;
+    files[CURR_NUM_FILES].writers=0;
+    CURR_NUM_FILES++;
+  }
+  printf("read %s of %lu bytes with %d readers and %d writers present\n", file_name, size, files[ind].readers, files[ind].writers);
+  
+  return;
+}
+
+void execute_write1_command(char *file_names){
+  char *filename1;  
+  char *filename2;  
+  sscanf(file_names, "%s %s", filename1, filename2);
+  FILE* file1 = fopen(filename1, "w");  // Open the first file in write mode
+  FILE* file2 = fopen(filename2, "r");  // Open the second file in read mode
+
+  int c;
+  while ((c = fgetc(file2)) != EOF) {
+    fputc(c, file1);
+  }
+
+  fclose(file1);
+  fclose(file2);
+
+  FILE *file = fopen(filename2, "rb"); // Open the file in binary read mode
+  // Seek to the end of the file and get the file size
+  fseek(file, 0, SEEK_END);
+  long size = ftell(file);
+  fclose(file);
+
+  int ind = update_writers(filename1);
+  if(ind==CURR_NUM_FILES)
+  {
+    files[CURR_NUM_FILES].filename=filename1;
+    files[CURR_NUM_FILES].readers=0;
+    files[CURR_NUM_FILES].writers=1;
+    CURR_NUM_FILES++;
+  }
+  printf("writing to %s added %lu bytes with %d readers and %d writers present\n", filename1, size, files[ind].readers, files[ind].writers);
+  
   return;
 }
 
